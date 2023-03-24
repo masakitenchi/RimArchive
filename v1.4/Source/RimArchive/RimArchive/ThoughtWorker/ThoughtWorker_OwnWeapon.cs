b4bcc1a228c1b6577c3d0eba331138ecf221e41d
Masakitenchi <fucong119@gmail.com>
2023-03-24 20:12:33 +0900

    Add a mood buff when holding certain weapon Test on Azusa

---
+++ v1.4/Source/RimArchive/RimArchive/ThoughtWorker/ThoughtWorker_OwnWeapon.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RimArchive;
+
+public class ThoughtWorker_OwnWeapon : ThoughtWorker
+{
+    protected override ThoughtState CurrentStateInternal(Pawn p)
+    {
+        return p.equipment.AllEquipmentListForReading.Exists(x => x.def == (p.kindDef as StudentDef).ownWeapon);
+    }
+}
+    
