ae5217d068df79c3342e55baf99b2cce1203017c
Masakitenchi <fucong119@gmail.com>
2023-03-28 19:03:38 +0900

    Initial commit. Only new classes Need to clear my mind for what exactly I want to achieve

---
+++ v1.4/Source/RimArchive/RimArchive/Abilities/Verb_StraightBeam.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RimArchive;
+public class Verb_StraightBeam : Verb_CastAbility
+{
+    //用OrbitStrike或者PowerBeam都会导致一个问题：放完了就是放完了
+    //我需要让这个技能成为一个持续施法的技能，放完之前要原地罚站
+    //但是还不能直接挪用原版的Verb到技能里，我真的哭死
+}
